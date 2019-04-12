e5f690614a95f72716f32af5b78461cf024e3883
Adriana Kopf <ak.adrianakopf@gmail.com>
2019-04-12 12:15:24 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -4,9 +4,9 @@
 <body>
 
   <p> Email sent from GitHub profile page.<p>
-  <p> Message from: <?php echo $_POST["Name"]; ?><br>
-      Email address: <?php echo $_POST["Email"]; ?> <br>
-      Message: <?php echo $_POST["Message"]; ?></p>
+  <p> Message from: <?php echo $_GET["Name"]; ?><br>
+      Email address: <?php echo $_GET["Email"]; ?> <br>
+      Message: <?php echo $_GET["Message"]; ?></p>
 
   
 </body>
